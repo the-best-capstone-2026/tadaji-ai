037a8ba1de8482da5062213fbcfee013208579d1
Dongju <dongju@tadanji.com>
2026-02-09 19:00:26 +0900

    feat: 이미지 업로드 로그 확인 구현 완료

---
+++ src/test/java/com/example/demo/음식사진분류ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class 음식사진분류ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
